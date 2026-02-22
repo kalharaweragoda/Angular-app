9f7b05c38f81f74ca0ca7d93e2a7974eea7aa7b3
kalharaweragoda <kalharaweragoda@gmail.com>
2026-02-22 18:17:13 +0530

    product package add

--- demo-app/src/app/app.routes.ts
+++ demo-app/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { AboutUs } from './pages/about-us/about-us';
+import { Product } from './pages/product/product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"",
+    component:Dashboard
+  },
+  {
+    path:"about-us",
+    component:AboutUs
+  },
+  {
+    path:'products',
+    component:Product
+  }
+];
